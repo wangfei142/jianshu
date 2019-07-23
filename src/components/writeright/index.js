676dc9fd5803f181235158b86465bc9fb8f6e0bc
ljy971221 <2530467704@qq.com>
2019-07-23 21:42:38 +0800

    完成文章发布页,数据库加入后，仓库重新获取

--- src/components/writeright/index.js
+++ src/components/writeright/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import styles from './index.less'
-import { Col, Button } from 'antd'
+import { Col, Button, message } from 'antd'
+import { connect } from 'dva'
 import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
+import { dispatch } from 'C:/Users/Shinelon/AppData/Local/Microsoft/TypeScript/3.5/node_modules/rxjs/internal/observable/range';
+import axios from 'axios'
 
 class WriteRight extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      text: '',
+      activeTitle: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(value) {
+    this.setState({ text: value })
+  }
 
   modules = {
     toolbar: [
@@ -26,7 +42,7 @@ class WriteRight extends React.Component {
     return (
       <Col span={16} className={styles.gutterRow}>
         <div>
-          <input type="text" className={styles.titleInpuut} />
+          <input type="text" value={this.activeTitle} onChange={this.handleChgTitle} className={styles.titleInpuut} />
         </div>
         <div className={styles.quillTxt}>
           <ReactQuill theme="snow"
@@ -34,14 +50,50 @@ class WriteRight extends React.Component {
             formats={this.formats}  //调用官方组件
             onChange={this.handleChange}  //事件函数
           >
-            <div className={styles.quillInput}></div>
           </ReactQuill>
         </div>
-        <Button type="primary" block>
+        <Button type="primary" block onClick={this.setActive} loading={this.state.loading}>
           发布文章
         </Button>
       </Col>
     )
   }
+  setActive = () => {   //发送文章请求
+    if (this.state.activeTitle == '' || this.state.text == '') {
+      message.warning('请输入内容');
+      return
+    }
+    this.setState({ loading: !this.state.loading })
+    let time = new Date().toLocaleDateString() + new Date().toLocaleTimeString()
+    let data = {
+      author: '李金益',
+      title: this.state.activeTitle,
+      data: this.state.text,
+      posted_time: time
+    }
+    axios.post('http://10.36.140.11:8080/api/article', JSON.stringify(data))
+      .then(response => {
+        this.setState({ loading: !this.state.loading })
+        message.success('发布成功');
+        this.props.getActiveList()   //重新get 仓库列表
+      }).catch(error => {
+        this.setState({ loading: !this.state.loading })
+        message.error('网络连接错误');
+      })
+  }
+
+
+  handleChgTitle = (e) => {
+    let value = e.currentTarget.value
+    this.setState({ activeTitle: value })
+  }
 }
-export default WriteRight
+
+
+export default connect(
+  null, {
+    getActiveList: () => ({ type: 'article/getActiveList' })
+  }
+)(WriteRight)
+
+
--- src/models/article.js
+++ src/models/article.js
@@ -1,9 +1,12 @@
+import axios from 'axios'
+import router from 'umi/router'
+
 // 文章 的 model
 export default {
   namespace: 'article',
 
   state: {
-    data: [
+    activeList: [
       {
         id: '1',
         author: 'John Brown',//作者
@@ -26,56 +29,25 @@ export default {
           content: 'New York No Park', //评论内容
         }]
       },
-      {
-        id: '2',
-        author: 'John Brown',
-        posted_time: "2018-12-25 12:05:09",
-        title: 'New York No. 2 Lake Park',
-        hot: '13300',  // 热度
-        discuss: '223', //  评论次数
-        like: '22', // 喜欢
-        data:
-          '现在很多外媒都争相报道英国王室，然而很多时候看到的只是王室的表面新闻，其实有很多的内幕都是人们所不熟知的。就比如说在年末梅根置装费问题闹的沸',  // 内容
-        comment: [{
-          id: '1',
-          username: 'John Brown', //评论者
-          retime: "2018-12-25 12:05:09", //评论时间
-          content: 'New York No. 1 Lake Park', //评论内容
-        }, {
-          id: '2',
-          username: 'John Brown', //评论者
-          retime: "2018-12-25 12:05:09", //评论时间
-          content: 'New York No. 2 Lake Park', //评论内容
-        }]
-      },
-      {
-        id: '3',
-        author: 'John Brown',
-        posted_time: "2018-12-25 12:05:09",
-        title: 'New York No. 1 Lake Park',
-        hot: '13314',  // 热度
-        discuss: '223', //  评论次数
-        like: '22', // 喜欢
-        data:
-          '现在很多外媒都争相报道英国王室，然而很多时候看到的只是王室的表面新闻，其实有很多的内幕都是人们所不熟知的。就比如说在年末梅根置装费问题闹的沸',  // 内容
-        comment: [{
-          id: '1',
-          username: 'John Brown', //评论者
-          retime: "2018-12-25 12:05:09", //评论时间
-          content: 'New York No. 1 Lake Park', //评论内容
-        }, {
-          id: '2',
-          username: 'John Brown', //评论者
-          retime: "2018-12-25 12:05:09", //评论时间
-          content: 'New York No. 1 Lake Park', //评论内容
-        }]
-      },
     ]
   },
 
 
+  effects: {  //暂且封印 用于文章发布后重新get仓库
+    *getActiveList(action, { put, select }) {
+      let response = yield axios.get('http://10.36.140.11:8080/api/article')
+      let res = response.data.data
+      yield put({ type: 'setActiveList', list: res })
+      router.push('/');
+    }
+  },
+
 
   reducers: {
-
+    setActiveList(state, action) {
+      return Object.assign({}, state, {
+        activeList: action.list
+      })
+    }
   }
 }
