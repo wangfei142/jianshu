4e59db9b3e5be3d893896b3ce2113e8d05d428b6
wangfei <wangfei@qq.com>
2019-07-24 10:21:48 +0800

    获取评论

--- src/models/article.js
+++ src/models/article.js
@@ -5,24 +5,43 @@ export default {
 
   state: {
     tableDataId: '',
-    activeList: []
+    activeList: [],  // 文章列表
+    commentList: [],  // 评论列表
   },
 
-
   effects: {
+
+    //获取评论
+    *getComment(action, { put }) {
+      let response = yield axios.get('http://10.36.140.11:8080/api/comment');
+      let res = yield response.data;
+      yield put({ type: 'setComment', commentList: res.data.list });
+    },
+    //删除评论
+    *delComment(action, { put }) {
+      let response = yield axios.delete(`http://10.36.140.11:8080/api/comment/${action.id}`);
+      let res = yield response.data;
+      yield put({ type: 'getComment' });
+    },
+    //获取文章
     *getArticle(action, { put }) {
       let response = yield axios.get('http://10.36.140.11:8080/api/article');
       let res = yield response.data;
       yield put({ type: 'setArticle', activeList: res.data.list });
+      yield put({ type: 'getComment' });
     },
+    //删除文章
     *delArticle(action, { put }) {
       let response = yield axios.delete(`http://10.36.140.11:8080/api/article/${action.id}`);
       let res = yield response.data;
       yield put({ type: 'getArticle' });
     },
+
+
   },
 
   reducers: {
+    // 设置文章列表
     setArticle(state, action) {
       return {
         ...state,
@@ -30,7 +49,15 @@ export default {
           activeList: action.activeList,
         },
       };
-
+    },
+    // 设置评论列表
+    setComment(state, action) {
+      return {
+        ...state,
+        ...{
+          commentList: action.commentList,
+        },
+      };
     },
     tableDataOperation(state, action) {
       console.log(action.tableDataId);
