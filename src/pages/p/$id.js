659837363e623086f9b4bb74a64a09bc6c20846c
ljy971221 <2530467704@qq.com>
2019-07-25 14:07:25 +0800

    完成评论区样式

--- src/pages/p/$id.js
+++ src/pages/p/$id.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from './index.less'
-import { Icon } from 'antd';
+import { Icon, Comment, Avatar, Input, Button } from 'antd';
 import { connect } from 'dva'
 import axios from 'axios'
 
+
+const { TextArea } = Input;
 class ActiveInit extends React.Component {
   state = {
     activeDate: {},
@@ -18,7 +20,10 @@ class ActiveInit extends React.Component {
           <div className={styles.author}>
             {/* 头像 */}
             <a href="#" className={styles.avatar}>
-              <img src={this.state.activeDate.avatar} alt="" />
+              <Avatar
+                size={48} icon="user"
+                src={this.state.activeDate.avatar}
+              />
             </a>
             <div className={styles.authorRigth}>
               {/* 昵称lv与按钮 */}
@@ -38,13 +43,46 @@ class ActiveInit extends React.Component {
               </div>
             </div>
           </div>
+          {/* 渲染带标签的文章内容 */}
           <div className={styles.contentDate} dangerouslySetInnerHTML={{ __html: this.state.contentDate }}></div>
-        </div>
+          {/* 评论 */}
+          <div className={styles.comment}>
+            {/* 发布评论 */}
+            <div className={styles.issueComment}>
+              <TextArea rows={4} placeholder='请输入你的看法' />
+              <div className={styles.commentBtn}>
+                <Button type="danger" block>
+                  发布文章
+                </Button>
+              </div>
+            </div>
+            {/* 评论 */}
+            <div className={styles.contentComment}>
+              <Comment
+                author={<a>Han Solo</a>}  //评论者
+                avatar={                  //头像
+                  <Avatar
+                    size={36} icon="user"
+                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                    alt="Han Solo"
+                  />
+                }
+                content={                 //评论内容
+                  <p>
+                    We supply a series of design principles, practical patterns and high quality design
+                    resources (Sketch and Axure), to help people create their product prototypes beautifully
+                    and efficiently.
+                  </p>
+                }
 
+              />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
-  removeHTMLTag = (str) => {
+  removeHTMLTag = (str) => {   //删除 HTML 标签，获取内容
     str = str.replace(/<\/?[^>]*>/g, ''); //去除HTML tag
     str = str.replace(/[ | ]*\n/g, '\n'); //去除行尾空白
     //str = str.replace(/\n[\s| | ]*\r/g,'\n'); //去除多余空行
@@ -59,7 +97,6 @@ class ActiveInit extends React.Component {
         this.setState({ activeDate: response.data.data })
         this.setState({ contentDate: response.data.data.data })
       }).catch(error => {
-        console.log(1)
       })
   }
 
