e39c3296ea2be09a36fd0c03cee0974e79cad396
God <2577914898@qq.com>
2019-07-25 20:04:30 +0800

    修改用户信息完成

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Upload } from 'antd';
 export default {
   namespace: 'user',
 
@@ -63,7 +64,106 @@ export default {
       var list = JSON.stringify(res.data);
       window.localStorage.setItem('loginMethod', list);
       yield put({ type: 'loginMethod', data: res });
+    },
+
+    //修改用户的信息的数据
+    *information(action, { put }) {
+      console.log(action.values.upload);
+      let loginMethod = JSON.parse(window.localStorage.getItem('loginMethod'));
+      // console.log(loginMethod._id)
+      //只修改昵称
+
+      if (!action.values.upload || action.values.upload.length == 0) {
+        //有名字 没头像的发送
+        // console.log("没头像");
+        if (action.values.username) {
+          let userInfo = {
+            nickname: action.values.username,
+          };
+          let obj = {
+            id: loginMethod._id,
+            userInfo
+          }
+          let response = yield axios.post(`http://10.36.140.11:8080/api/user/amend`, JSON.stringify(obj));
+          let res = yield response.data;
+          let list = JSON.stringify(Object.assign({}, loginMethod, {
+            nickname: res.data.nickname,
+          }))
+          window.localStorage.setItem('loginMethod', list);
+          window.location.reload();
+        }
+      } else {
+        if (!action.values.username) {
+          //没名字 有头像的发送
+          // console.log("没名字");
+          let loginMethod = JSON.parse(window.localStorage.getItem('loginMethod'));
+          // new formData():将form表单元素的name与value进行组合，实现表单数据的序列化，从而减少表单元素的拼接，提高工作效率。
+          let formData = new FormData();
+          // //追加数据
+          formData.append("id", loginMethod._id);
+          // //获取 document ，  action.values.upload[0]， 文件上传的路径并追加起来
+          //发送的数据 action.values.upload[0].originFileObj
+          formData.append("avatar", action.values.upload[0].originFileObj);
+          let response = yield axios.post(`http://10.36.140.11:8080/api/user/updateavatar`,
+            formData
+          );
+          let res = yield response.data;
+
+          let list = JSON.stringify(Object.assign({}, loginMethod, {
+            avatar: res.data,
+          }))
+          window.localStorage.setItem('loginMethod', list);
+          window.location.reload();
+        } else {
+          // console.log("有头像");
+          // console.log("有名字");
+          let loginMethod = JSON.parse(window.localStorage.getItem('loginMethod'));
+          let formData = new FormData();
+          formData.append("id", loginMethod._id);
+          formData.append("avatar", action.values.upload[0].originFileObj);
+          let portrait = yield axios.post(`http://10.36.140.11:8080/api/user/updateavatar`,
+            formData
+          );
+          // 头像
+          let por = yield portrait.data.data;
+
+          let userInfo = {
+            nickname: action.values.username,
+          };
+          let obj = {
+            id: loginMethod._id,
+            userInfo
+          }
+          let response = yield axios.post(`http://10.36.140.11:8080/api/user/amend`, JSON.stringify(obj));
+          let res = yield response.data;
+
+          let list = JSON.stringify(Object.assign({}, loginMethod, {
+            nickname: res.data.nickname,
+            avatar: por,
+          }))
+
+
+          window.localStorage.setItem('loginMethod', list);
+          window.location.reload();
+
+
+
+
+
+
+        }
+
+
+
+      }
+      console.log("仓库的数据", action.values);
+      console.log("仓库的数据", action.values.upload[0]);
+
+
+
     }
+
+
 
   },
 
